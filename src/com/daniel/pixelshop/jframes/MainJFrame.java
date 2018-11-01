1d266cae94a01d7b1628b06e02d84cc503d0b356
DanielChapin <dchapes123@gmail.com>
2018-11-01 16:52:20 -0500

    Added MainJFrame Class

---
+++ src/com/daniel/pixelshop/jframes/MainJFrame.java
@@ -0,0 +1,12 @@
+package com.daniel.pixelshop.jframes;
+
+import javax.swing.JFrame;
+
+public class MainJFrame extends JFrame {
+	
+	public MainJFrame() {
+		setSize(640, 480);
+		setVisible(true);
+	}
+
+}
