b62a0999bcb26432d8bd5dd0164b374d1a679bb2
DanielChapin <dchapes123@gmail.com>
2018-11-01 16:05:36 -0500

    Added JOGL platform files for mac and windows x64

--- README.md
+++
@@ -1,2 +0,0 @@
-# DCPixelShop
-Software project I'm making that will allow for people to make 8-bit pixel art.
--- src/com/daniel/pixelshop/main/DCPSMain.java
+++ src/com/daniel/pixelshop/main/DCPSMain.java
@@ -7,7 +7,7 @@ package com.daniel.pixelshop.main;
 public class DCPSMain {
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
